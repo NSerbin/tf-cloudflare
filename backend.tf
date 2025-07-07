706b5721419e0ebc9af58bb8d39ea3a3af9840a3
Nicolás Serbin <55348850+NSerbin@users.noreply.github.com>
2025-07-08 00:47:27 +0100

    fix 2 typos

--- backend.tf
+++ backend.tf
@@ -31,7 +31,7 @@ resource "cloudflare_zero_trust_access_application" "backend_app" {
 ## Record for Backend
 resource "cloudflare_dns_record" "backend_record" {
   zone_id = cloudflare_zone.nserbin_website_zone.id
-  name    = var.backend["name"]}.var.nserbin_website["domain"]
+  name    = var.backend["name"].var.nserbin_website["domain"]
   content = var.raspberry_pi_tunnel["record"]
   type    = var.dns_records["type"]
   ttl     = var.dns_records["ttl"]
